ab4baa4c9d4762ece279963d5af55d94408ab66f
smcintyre95 <stephenmcintyree@gmail.com>
2020-03-06 13:22:33 +0000

    Exam Stephen McIntyre.

---
+++ src/main/java/ie/gmit/Employee.java
@@ -0,0 +1,87 @@
+/*
+
+Stephen McIntyre
+g00325469@gmit.ie
+
+Creating arguments that may cause a test case to be passed or failed.
+Generating our variables.
+
+ */
+
+package ie.gmit;
+
+ class Employee {
+
+    private String title;
+    private String name;
+    private String PPS;
+    private String number;
+
+     String getTitle() {
+        return title;
+    }
+
+     String getName() {
+        return name;
+    }
+
+     String getPPS() {
+        return PPS;
+    }
+
+     String getNumber() {
+        return number;
+    }
+
+     String getEmploymentType() {
+        return employmentType;
+    }
+
+     int getAge() {
+        return age;
+    }
+
+    private String employmentType;
+    private int age;
+
+     Employee(String title, String name, String PPS, String number, String employmentType, int age) {
+
+        if (title.equals("Mr") || title.equals("Mrs")  || title.equals("Miss")) {
+            this.title = title;
+        }
+        else
+            throw  new IllegalArgumentException("Title must be MR,Mrs ir Miss");
+
+        if(name.length() >= 5 & name.length()<=22){
+            this.name = name;
+        }
+        else
+            throw  new IllegalArgumentException("Please enter Valid name or name Size");
+
+        if(PPS.length() == 6){
+            this.PPS = PPS;
+        }
+        else
+            throw  new IllegalArgumentException("Please enter Valid PPS number");
+
+        if(number.length() == 7){
+            this.number = number;
+        }
+        else
+            throw new IllegalArgumentException("Please enter Valid phone number");
+
+        if(employmentType.equals("Full-time") || employmentType.equals("Part-time")){
+            this.employmentType = employmentType;
+        }
+
+        else
+            throw new IllegalArgumentException("Please enter Valid employment title");
+
+        if(age <= 18){
+            throw new IllegalArgumentException("Under age, Too young to work here. must be 18 or over");
+        }
+        else
+            this.age = age;
+    }
+
+}
---
+++ src/test/java/ie/gmit/EmployeeTest.java
@@ -0,0 +1,93 @@
+
+/*
+
+Stephen McIntyre
+g00325469@gmit.ie
+
+Testing each possible case of incorrect data being entered into employee database
+
+ */
+
+
+package ie.gmit;
+
+import org.junit.jupiter.api.*;
+
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class EmployeeTest {
+
+
+    @BeforeEach
+    void init(TestInfo testInfo, TestReporter testReporter){
+        testReporter.publishEntry("Testing" + " ", testInfo.getDisplayName());
+    }
+
+    @Test
+    @DisplayName("Testing test Constructor")
+     void testConstructor(){
+
+        Employee myEmployee = new Employee("Mr", "Stephen", "123456","1234567","Part-time",24);
+        assertEquals("Mr",myEmployee.getTitle());
+
+    }
+
+
+    @DisplayName("Testing for no Title")
+    @Test
+     void testConstructorNoTitle(){
+
+        assertThrows(IllegalArgumentException.class, () -> new Employee("","Stephen","123456","1234567","Part-time",24));
+
+
+    }
+
+    @DisplayName("Testing for Name")
+    @Test
+     void testConstructorNoName(){
+
+        assertThrows(IllegalArgumentException.class, () -> new Employee("Mr","Ste","123456","1234567","Part-time",24));
+
+
+    }
+
+    @DisplayName("Testing for PPS")
+    @Test
+     void testConstructorNoPPS(){
+
+        assertThrows(IllegalArgumentException.class, () -> new Employee("Mr","Stephen","12","1234567","Part-time",24));
+
+
+    }
+
+    @DisplayName("Testing for phone number")
+    @Test
+     void testConstructorNoNumber(){
+
+        assertThrows(IllegalArgumentException.class, () -> new Employee("Mr","Stephen","123456","123","Part-time",24));
+
+    }
+
+    @DisplayName("Testing for employement details")
+    @Test
+     void testConstructorNoEmployment(){
+
+        assertThrows(IllegalArgumentException.class, () -> new Employee("Mr","Stephen","123456","1234567","jobless",24));
+
+    }
+
+    @DisplayName("Testing for Age")
+    @Test
+     void testConstructorAge(){
+
+        assertThrows(IllegalArgumentException.class, () -> new Employee("Mr","Stephen","123456","1234567","jobless",15));
+
+    }
+
+
+
+
+
+}
